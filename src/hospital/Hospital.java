79af65dbe9bc223a31bcd4015579e81f8508aea0
julie <julie@Mattis>
2016-11-18 14:42:35 +0100

    Progress

--- src/hospital/Hospital.java
+++ src/hospital/Hospital.java
@@ -1,13 +1,14 @@
 
 package hospital;
 
-
+import hospital.Roster;
 
 public class Hospital {
 
     public static void main(String[] args) {
 
-        
+        Roster r = new Roster('A');
+        r.readShiftSystem();
     }
     
 }
